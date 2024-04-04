d16306da7d11e24dd282b713262a1bdbead909ad
NikolayBoyzz <123127020+NikolayBoyzz@users.noreply.github.com>
2024-04-05 00:52:24 +0300

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,11 @@
+from pydantic_setting import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
